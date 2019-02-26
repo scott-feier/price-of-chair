434bdf230f49cc083254b503df586fa35a91e1a5
scott-feier <scott.feier@gmail.com>
2019-02-26 15:16:56 -0500

    corrected database name

--- src/common/database.py
+++ src/common/database.py
@@ -9,7 +9,8 @@ class Database(object):
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.URI)
-        Database.DATABASE = client['fullstack']
+#        Database.DATABASE = client['fullstack']
+        Database.DATABASE = client['heroku_ck92mn9h']
 
     @staticmethod
     def insert(collection, data):
